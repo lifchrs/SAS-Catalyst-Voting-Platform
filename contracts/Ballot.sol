dce94d677241b4fc0ca6b712182b5fcb73e404e1
Adhitya Polavaram <polavaramadhitya@gmail.com>
2022-05-11 09:38:51 +0800

    fix ballot.json

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -46,11 +46,6 @@ contract Ballot{
     );
 */
 
-    constructor() public{
-        createProposal("Should SAS renovate their library?", "Here is a reason for this proposal.Here is a reason for this proposal.Here is a reason for this proposal.Here is a reason for this proposal.Here is a reason for this proposal.","propHash1");
-        createProposal("Should athletics be de-emphasised?", "At our school, we are constantly told about the accomplishments of IASAS athletes. We have pep rallies, and IASAS athletes are given red bags. In one of the house assemblies, all athletes were asked to stand up, for their different sports. Imagine what if felt like for those who didn’t take part in athletics, when they were the only people standing up. This would be fine if they were recognized, but quite frankly, they aren’t. We are constantly asked to “Support our Eagles” at athletic events, but for CulCon, nobody really cares. This issue is further worsened with the Scholars Luncheon. The GPA requirement is 3.7, which is low enough for a great number of people to qualify for it. But for those who aren’t as good academically, they would feel extremely bad when half the school goes to the gym one lunch. Furthermore, those who have very good GPAs, like having above a 4.1, can’t feel recognized because they are being recognized with half the school, “diluting” their recognition.","propHash2");
-        addVoter(0x0D3FE60dDDcaD85B534EA8b291cd7bc04bc5f3ef);
-    }
 
     function addVoter(address add) public{
         voterAdded[add] = true;
